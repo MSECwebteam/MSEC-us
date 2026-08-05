9b07d085e8d8a2d739b3bd84d712808653963036
MSECwebteam <msec.webteam@gmail.com>
2026-08-05 09:24:49 -0500

    JStestpush

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.querySelector(".mobile-menu");
 
+console.log("main.js loaded");
+
 menuToggle.addEventListener("click", () => {
+
+    console.log("Hamburger clicked");
 
     mobileMenu.classList.toggle("active");
 
