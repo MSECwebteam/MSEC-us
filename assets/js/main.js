cd0fffaaf192eaee2ccb269d6026ed787a4f64be
MSECwebteam <msec.webteam@gmail.com>
2026-08-05 09:53:03 -0500

    MenuCSSandJSFIX

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,11 @@ window.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle");
     const mobileMenu = document.querySelector(".mobile-menu");
 
-    console.log("Button:", menuToggle);
-    console.log("Menu:", mobileMenu);
-
     menuToggle.addEventListener("click", () => {
 
-        console.log("CLICKED!");
+        mobileMenu.classList.toggle("active");
 
-        mobileMenu.style.opacity = "1";
-        mobileMenu.style.visibility = "visible";
-        mobileMenu.style.transform = "translateY(0)";
-        mobileMenu.style.display = "flex";
+        console.log(mobileMenu.className);
 
     });
 
