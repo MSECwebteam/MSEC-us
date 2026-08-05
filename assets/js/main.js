ed01d138a66537f525b4ddc88b23ec0542561e13
MSECwebteam <msec.webteam@gmail.com>
2026-08-05 09:56:05 -0500

    JSfixHomeClose

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const menuToggle = document.querySelector(".menu-toggle");
     const mobileMenu = document.querySelector(".mobile-menu");
+    const mobileLinks = document.querySelectorAll(".mobile-menu a");
 
     menuToggle.addEventListener("click", () => {
 
         mobileMenu.classList.toggle("active");
 
-        console.log(mobileMenu.className);
+    });
+
+    mobileLinks.forEach(link => {
+
+        link.addEventListener("click", () => {
+
+            mobileMenu.classList.remove("active");
+
+        });
 
     });
 
